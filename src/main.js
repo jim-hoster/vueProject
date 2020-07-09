d956b3213811b9982f8605aeb9c27276d743fc2f
jim-hoster <1539663162@qq.com>
2020-07-10 01:21:15 +0800

    提交

--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+// 引入eleui框架
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+//引入Echarts，
+import echarts from 'echarts'
+// 引入iconfont字体库
+import './assets/font/iconfont.css'
+// 初始化样式
+import './assets/css/reset.min.css'
+import './assets/css/common.less'
+Vue.prototype.$echarts = echarts //定义为全局变量
+Vue.use(ElementUI)
 Vue.config.productionTip = false
-
 new Vue({
   router,
   render: h => h(App)
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,108 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
+// 登录与布局路由
+import Login from '../views/Login.vue'
+import Layout from '../views/Layout.vue'
 Vue.use(VueRouter)
+// 主页只引入独立业务的模块，其他使用懒加载
+const routes = [{
+    path: '/login',
+    component: Login
+  },
 
-  const routes = [
+  // 后端布局首页
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    component: Layout,
+    redirect: '/home',
+    children: [{
+      path: '/home',
+      component: () =>
+        import('../views/Layout/right/content/Home/Home.vue')
+    }, ]
   },
+
+  // 订单管理懒加载
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/order',
+    component: Layout,
+    children: [{
+      path: '/order',
+      component: () =>
+        import('../views/Layout/right/content/Order/Order.vue')
+    }]
+  },
+
+  // 商品管理路由懒加载
+  {
+    path: '/product',
+    component: Layout,
+    children: [{
+        path: '/product/product-list',
+        component: () =>
+          import('../views/Layout/right/content/Product/ProductList.vue')
+      }, {
+        path: '/product/add-product',
+        component: () =>
+          import('../views/Layout/right/content/Product/AddProduct.vue')
+      },
+      {
+        path: '/product/product-class',
+        component: () =>
+          import('../views/Layout/right/content/Product/ProductClass.vue')
+      },
+    ]
+  },
+
+  // 店铺管理懒加载
+  {
+    path: '/shop',
+    component: Layout,
+    children: [{
+      path: '/shop',
+      component: () =>
+        import('../views/Layout/right/content/Shop/Shop.vue')
+    }]
+  },
+
+  // 账号管理
+  {
+    path: '/account',
+    component: Layout,
+    children: [{
+        path: '/account/account-list',
+        component: () =>
+          import('../views/Layout/right/content/Account/AccountList.vue')
+      }, {
+        path: '/account/add-account',
+        component: () =>
+          import('../views/Layout/right/content/Account/AddAccount.vue')
+      },
+      {
+        path: '/account/change-account',
+        component: () =>
+          import('../views/Layout/right/content/Account/ChangeAccount.vue')
+      },
+    ]
+  },
+
+  // 销售统计
+  {
+    path: '/sales',
+    component: Layout,
+    children: [{
+        path: '/sales/product-sale',
+        component: () =>
+          import('../views/Layout/right/content/Sales/ProductSales.vue')
+      },
+      {
+        path: '/sales/order-sale',
+        component: () =>
+          import('../views/Layout/right/content/Sales/OrderSales.vue')
+      },
+    ]
+  },
 ]
 
 const router = new VueRouter({
